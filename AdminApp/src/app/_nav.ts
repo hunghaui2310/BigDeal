11da9457bb0656adb8c8f8bb17f4ac7160a52e53
hunghaui2310 <hunghaui2310@gmail.com>
2020-11-08 10:53:02 +0700

    add template for admin

---
+++ AdminApp/src/app/_nav.ts
@@ -0,0 +1,114 @@
+import { INavData } from '@coreui/angular';
+
+export const navItems: INavData[] = [
+  {
+    name: 'Dashboard',
+    url: '/dashboard',
+    icon: 'icon-speedometer',
+    badge: {
+      variant: 'info',
+      text: 'NEW'
+    }
+  },
+  {
+    title: true,
+    name: 'Theme'
+  },
+  {
+    title: true,
+    name: 'Admin management'
+  },
+  {
+    name: 'Base',
+    url: '/base',
+    icon: 'icon-puzzle',
+  },
+  {
+    name: 'Buttons',
+    url: '/buttons',
+    icon: 'icon-cursor',
+  },
+  {
+    name: 'Charts',
+    url: '/charts',
+    icon: 'icon-pie-chart'
+  },
+  {
+    name: 'Icons',
+    url: '/icons',
+    icon: 'icon-star'
+  },
+  {
+    name: 'Notifications',
+    url: '/notifications',
+    icon: 'icon-bell',
+  },
+  {
+    name: 'Widgets',
+    url: '/widgets',
+    icon: 'icon-calculator',
+    badge: {
+      variant: 'info',
+      text: 'NEW'
+    }
+  },
+  {
+    divider: true
+  },
+  {
+    title: true,
+    name: 'Extras',
+  },
+  {
+    name: 'Pages',
+    url: '/pages',
+    icon: 'icon-star',
+    children: [
+      {
+        name: 'Login',
+        url: '/login',
+        icon: 'icon-star'
+      },
+      {
+        name: 'Register',
+        url: '/register',
+        icon: 'icon-star'
+      },
+      {
+        name: 'Error 404',
+        url: '/404',
+        icon: 'icon-star'
+      },
+      {
+        name: 'Error 500',
+        url: '/500',
+        icon: 'icon-star'
+      }
+    ]
+  },
+  {
+    name: 'Disabled',
+    url: '/dashboard',
+    icon: 'icon-ban',
+    badge: {
+      variant: 'secondary',
+      text: 'NEW'
+    },
+    attributes: { disabled: true },
+  },
+  {
+    name: 'Download CoreUI',
+    url: 'http://coreui.io/angular/',
+    icon: 'icon-cloud-download',
+    class: 'mt-auto',
+    variant: 'success',
+    attributes: { target: '_blank', rel: 'noopener' }
+  },
+  {
+    name: 'Try CoreUI PRO',
+    url: 'http://coreui.io/pro/angular/',
+    icon: 'icon-layers',
+    variant: 'danger',
+    attributes: { target: '_blank', rel: 'noopener' }
+  }
+];
